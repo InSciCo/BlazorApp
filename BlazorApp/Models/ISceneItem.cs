03b1003b37966cdfd70aab57d170fb1aa714f8b6
Thomas May <tjmay@inscico.com>
2023-01-20 17:08:44 -0800

    math demo

---
+++ BlazorApp/Models/ISceneItem.cs
@@ -0,0 +1,13 @@
+﻿namespace BlazorApp
+{
+    public interface ISceneItem
+    {
+        double Height { get; set; }
+        double HeightOffFloor { get; set; }
+        string ImageURL { get; set; }
+        string Name { get; set; }
+        ScenePos ScenePos { get; set; }
+        double Width { get; set; }
+
+    }
+}
