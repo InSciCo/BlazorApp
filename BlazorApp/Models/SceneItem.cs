cb9d24a330bd92c95c8b987b318a6f6aa7e59c37
Thomas May <tjmay@inscico.com>
2023-01-20 19:39:39 -0800

    class fucky 2

--- BlazorApp/Models/SceneItem.cs
+++ BlazorApp/Models/SceneItem.cs
@@ -7,26 +7,7 @@ using static BlazorApp.AnnotatedImagePage;
 
 namespace BlazorApp;
 
-public class SceneGraph
-{
-    public string ImageURL { get; set; }
-    public double ImageRatio { get; set; } // 16:9 etc. 
-
-    // these values are in ft
-    public double Depth { get; set; } // Distance Camera to back wall
-    public double CameraHeight { get; set; }
-    public Dictionary<string, SceneItem> SceneItems { get; set; } = new();
-}
-
-public class ScenePos
-{
-    // these values are in ft
-    public double X { get; set; } // floor left to right
-    public double Y { get; set; } // floor front to back 
-    public double Z { get; set; } // floor up
-}
-
-public record SceneItem
+public record SceneItem : ISceneItem
 {
     public string Name { get; set; }
     public string ImageURL { get; set; }
